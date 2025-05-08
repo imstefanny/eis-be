5c2b620c9deb552b6e7c2a92b65750712abe29be
stefanny <stefanny@witech.co.id>
2025-05-08 22:12:21 +0700

    [UPD] add jwt token to user login

--- dto/users.go
+++ dto/users.go
@@ -11,3 +11,11 @@ type LoginUsersRequest struct {
 	Email			string			`json:"email"`
 	Password		string			`json:"password"`
 }
+
+type LoginUsersResponse struct {
+	ID			 	uint			`json:"id"`
+	Name			string			`json:"name"`
+	Email			string			`json:"email"`
+	Token			string			`json:"token"`
+	RoleID			uint			`json:"role_id"`
+}
--- usecase/users.go
+++ usecase/users.go
@@ -5,6 +5,7 @@ import (
 	"eis-be/repository"
 	"eis-be/dto"
 	"eis-be/helpers"
+	"eis-be/middlewares"
 	"reflect"
 	"errors"
 )
@@ -83,5 +84,19 @@ func (s *usersUsecase) Login(data dto.LoginUsersRequest) (interface{}, error) {
 		return nil, err
 	}
 
-	return user, nil
+	token, err := middlewares.CreateToken(user.Email, user.Password)
+
+	if err != nil {
+		return nil, err
+	}
+
+	userResponse := dto.LoginUsersResponse{
+		ID		: user.ID,
+		Name 	: user.Name,
+		Email	: user.Email,
+		Token 	: token,
+		RoleID	: user.RoleID,
+	}
+
+	return userResponse, nil
 }
