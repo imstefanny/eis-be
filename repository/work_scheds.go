c1a89a13ad2631450968a818d4985e698e1bdded
stefanny <stefanny@witech.co.id>
2025-05-14 13:59:15 +0700

    [UPD] workscheds: add delete method

--- repository/work_scheds.go
+++ repository/work_scheds.go
@@ -10,7 +10,7 @@ type WorkSchedsRepository interface {
 	GetAll() ([]models.WorkScheds, error)
 	Create(workScheds models.WorkScheds) error
 	Find(id int) (models.WorkScheds, error)
-	Update(id int, blog models.WorkScheds) error
+	Update(id int, workSched models.WorkScheds) error
 	Delete(id int) error
 }
 
@@ -39,15 +39,15 @@ func (r *workSchedsRepository) Create(workScheds models.WorkScheds) error {
 }
 
 func (r *workSchedsRepository) Find(id int) (models.WorkScheds, error) {
-	blog := models.WorkScheds{}
-	if err := r.db.Preload("Details").First(&blog, id).Error; err != nil {
-		return blog, err
+	workSched := models.WorkScheds{}
+	if err := r.db.Preload("Details").First(&workSched, id).Error; err != nil {
+		return workSched, err
 	}
-	return blog, nil
+	return workSched, nil
 }
 
-func (r *workSchedsRepository) Update(id int, blog models.WorkScheds) error {
-	query := r.db.Model(&blog).Updates(blog)
+func (r *workSchedsRepository) Update(id int, workSched models.WorkScheds) error {
+	query := r.db.Model(&workSched).Updates(workSched)
 	if err := query.Error; err != nil {
 		return err
 	}
@@ -55,8 +55,18 @@ func (r *workSchedsRepository) Update(id int, blog models.WorkScheds) error {
 }
 
 func (r *workSchedsRepository) Delete(id int) error {
-	blog := models.WorkScheds{}
-	if err := r.db.Delete(&blog, id).Error; err != nil {
+	workSched := models.WorkScheds{}
+	workSchedDetail := models.WorkSchedDetails{}
+	err := r.db.Transaction(func(tx *gorm.DB) error {
+        if err := tx.Where("work_sched_id = ?", id).Delete(&workSchedDetail).Error; err != nil {
+            return err
+        }
+        if err := tx.Where("id = ?", id).Delete(&workSched).Error; err != nil {
+            return err
+        }
+        return nil
+    })
+	if err != nil {
 		return err
 	}
 	return nil
