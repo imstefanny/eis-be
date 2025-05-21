a86ae7cfbcb467e14e780d93464e18ab51e0cd59
imstefanny <imstefannyy@gmail.com>
2025-05-21 23:57:18 +0700

    [UPD] documents: add object to relation fields

--- repository/documents.go
+++ repository/documents.go
@@ -1,7 +1,6 @@
 package repository
 
 import (
-	"eis-be/dto"
 	"eis-be/models"
 	"strings"
 
@@ -9,7 +8,7 @@ import (
 )
 
 type DocumentsRepository interface {
-	Browse(page, limit int, search string) ([]dto.DocumentsResponse, int64, error)
+	Browse(page, limit int, search string) ([]models.Documents, int64, error)
 	Create(documents models.Documents) error
 	Find(id int) (models.Documents, error)
 	FindByApplicantId(id int) ([]models.Documents, error)
@@ -25,21 +24,16 @@ func NewDocumentsRepository(db *gorm.DB) *documentsRepository {
 	return &documentsRepository{db}
 }
 
-func (r *documentsRepository) Browse(page, limit int, search string) ([]dto.DocumentsResponse, int64, error) {
-	var documents []dto.DocumentsResponse
+func (r *documentsRepository) Browse(page, limit int, search string) ([]models.Documents, int64, error) {
+	var documents []models.Documents
 	var total int64
 	offset := (page - 1) * limit
 	search = "%" + strings.ToLower(search) + "%"
-	if err := r.db.
-		Joins("JOIN doc_types dt ON dt.id = documents.type_id").
-		Select("documents.*, dt.name AS type_name").
-		Where("LOWER(type_id) LIKE ?", search).
-		Limit(limit).Offset(offset).
-		Find(&documents).Error; err != nil {
+	if err := r.db.Where("LOWER(name) LIKE ?", search).Limit(limit).Offset(offset).Preload("Type").Preload("Applicant").Preload("Student").Find(&documents).Error; err != nil {
 		return nil, 0, err
 	}
 
-	if err := r.db.Model(&dto.DocumentsResponse{}).Where("LOWER(type_id) LIKE ?", search).Count(&total).Error; err != nil {
+	if err := r.db.Model(&models.Documents{}).Where("LOWER(name) LIKE ?", search).Count(&total).Error; err != nil {
 		return nil, 0, err
 	}
 	return documents, total, nil
@@ -55,7 +49,7 @@ func (r *documentsRepository) Create(documents models.Documents) error {
 
 func (r *documentsRepository) Find(id int) (models.Documents, error) {
 	document := models.Documents{}
-	if err := r.db.First(&document, id).Error; err != nil {
+	if err := r.db.Preload("Type").Preload("Applicant").Preload("Student").First(&document, id).Error; err != nil {
 		return document, err
 	}
 	return document, nil
@@ -63,7 +57,7 @@ func (r *documentsRepository) Find(id int) (models.Documents, error) {
 
 func (r *documentsRepository) FindByApplicantId(id int) ([]models.Documents, error) {
 	documents := []models.Documents{}
-	if err := r.db.Where("applicant_id = ?", id).Find(&documents).Error; err != nil {
+	if err := r.db.Where("applicant_id = ?", id).Preload("Type").Preload("Applicant").Preload("Student").Find(&documents).Error; err != nil {
 		return nil, err
 	}
 	return documents, nil
