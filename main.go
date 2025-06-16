81a84167b4b3bd472707531e5ec8f57ef633e5b9
felicio.angga <211110176@students.mikroskil.ac.id>
2025-06-16 15:08:53 +0700

    fix: add eis letjen to allow origin

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 
 	e := echo.New()
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins:     []string{"http://localhost:5173", "https://landing-page-school.web.app", "http://localhost:5174"},
+		AllowOrigins:     []string{"https://landing-page-school.web.app", "https://eis-letjen.web.app/", "http://localhost:5174", "http://localhost:5173"},
 		AllowMethods:     []string{echo.GET, echo.POST, echo.PUT, echo.DELETE, echo.OPTIONS},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		AllowCredentials: true,
