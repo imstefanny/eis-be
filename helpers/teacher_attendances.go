135ccb0b8e9d85709cc37c02448e2a0ee8ddf0d2
stefanny <stefanny@witech.co.id>
2025-06-27 12:58:08 +0700

    [FIX] teacheratts: bug computing the late state

--- helpers/teacher_attendances.go
+++ helpers/teacher_attendances.go
@@ -2,20 +2,24 @@ package helpers
 
 import (
 	"eis-be/models"
+	"fmt"
 	"strings"
 	"time"
 )
 
 func TeacherAttsRemark(att models.TeacherAttendances, workSched models.WorkScheds) string {
+	fmt.Println("\nTeacher Attendance Remark Calculation for:", att.DisplayName, "on :", workSched.Name)
 	dayOfWeek := att.Date.Weekday()
 	remark := []string{}
 	for _, schedule := range workSched.Details {
 		date := att.Date
 		loc, _ := time.LoadLocation("Asia/Jakarta")
-		parseInTime, _ := time.Parse("15:04:05", schedule.WorkStart)
-		parseOutTime, _ := time.Parse("15:04:05", schedule.WorkEnd)
+
+		parseInTime, _ := time.Parse("15:04", schedule.WorkStart)
+		parseOutTime, _ := time.Parse("15:04", schedule.WorkEnd)
 		work_start := time.Date(date.Year(), date.Month(), date.Day(), parseInTime.Hour(), parseInTime.Minute(), parseInTime.Second(), 0, loc)
 		work_end := time.Date(date.Year(), date.Month(), date.Day(), parseOutTime.Hour(), parseOutTime.Minute(), parseOutTime.Second(), 0, loc)
+
 		if schedule.Day == dayOfWeek.String() {
 			if att.LogInTime.After(work_start) {
 				remark = append(remark, "Terlambat")
